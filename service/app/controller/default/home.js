16a74c00f35bae7f7f365154af006378a86b8f64
wsdmzy <949328973@qq.com>
2020-05-03 21:34:07 +0800

    后端

--- service/app/controller/default/home.js
+++ service/app/controller/default/home.js
@@ -4,8 +4,68 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    this.ctx.body = 'aa';
+    this.ctx.body = 'hello';
   }
+  // FROM_UNIXTIME(article.addtime, "%Y-%m-%d %H:%i:%s")
+  // 文章列表
+  async getArticleList() {
+    const sql = 'SELECT article.id as id,' +
+                'article.title as title,' +
+                'article.introduce as introduce,' +
+                'article.addtime as addtime,' +
+                'article.view_count as view_count,' +
+                'type.typename as typename ' +
+                'FROM article LEFT JOIN type ON article.type_id=type.id';
+    const results = await this.app.mysql.query(sql);
+    this.ctx.body = {
+      data: results,
+    };
+  }
+
+  // 根据id获取文章详情
+  async getArticleById() {
+    const id = this.ctx.params.id;
+    const sql = 'SELECT article.id as id,' +
+    'article.title as title,' +
+    'article.introduce as introduce,' +
+    'article.article_content as article_content,' +
+    'article.addtime as addtime,' +
+    'article.view_count as view_count,' +
+    'type.typename as typename,' +
+    'type.id as typeid ' +
+    'FROM article LEFT JOIN type ON article.type_id=type.id ' +
+    'WHERE article.id = ' + id;
+    const result = await this.app.mysql.query(sql);
+    this.ctx.body = {
+      data: result,
+    };
+  }
+
+  // 得到类别名称和编号
+  async getTypeInfo() {
+    const result = await this.app.mysql.select('type');
+    this.ctx.body = {
+      data: result,
+    };
+  }
+
+  // 根据id获得文章列表
+  async getListById() {
+    const id = this.ctx.params.id;
+    const sql = 'SELECT article.id as id,' +
+                'article.title as title,' +
+                'article.introduce as introduce,' +
+                'article.addtime as addtime,' +
+                'article.view_count as view_count,' +
+                'type.typename as typename ' +
+                'FROM article LEFT JOIN type ON article.type_id=type.id ' +
+                'WHERE type_id =' + id;
+    const results = await this.app.mysql.query(sql);
+    this.ctx.body = {
+      data: results,
+    };
+  }
+
 }
 
 module.exports = HomeController;
--- service/app/router/default.js
+++ service/app/router/default.js
@@ -3,4 +3,8 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/default/index', controller.default.home.index);
+  router.get('/default/getarticlelist', controller.default.home.getArticleList);
+  router.get('/default/getarticlebyid/:id', controller.default.home.getArticleById);
+  router.get('/default/gettypeinfo', controller.default.home.getTypeInfo);
+  router.get('/default/getlistbyid/:id', controller.default.home.getListById);
 };
