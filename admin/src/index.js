44d1a21b5afa49cc12548fab7bce6ec05ecfdab0
wsdmzy <949328973@qq.com>
2020-05-03 21:33:45 +0800

    后台

---
+++ admin/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css'
+import Main from './pages/Main';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
